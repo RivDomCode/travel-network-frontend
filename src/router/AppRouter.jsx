4f732436998b39a6a77b32725194f0e6f9af6f12
unknown <pedrorivdom@gmail.com>
2022-12-14 19:51:58 +0100

    routing

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,39 @@
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Navigate
+  } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Profile from "../pages/Profile";
+import Register from "../pages/Register";
+
+
+export const AppRouter = () =>{
+
+    const authStatus = "non-auth";
+    return(
+        <BrowserRouter>
+        <Routes>
+            {
+                (authStatus === "non-auth") ? (
+                    <>
+                        <Route path="/login/*" element={<Login/>} />
+                        <Route path="/*" element={ <Navigate to="/login" /> } />
+                        <Route path="/register/" element={<Register/>} />
+                    </>
+                ) : (
+                    <>
+                        <Route path="/" element={<Home/>} />
+                        <Route path="/*" element={ <Navigate to="/" /> } />
+                        <Route path="/Profile/" element={<Profile/>} />
+                    </>
+                )
+            }
+        </Routes>
+    </BrowserRouter>
+    )
+
+
+}
