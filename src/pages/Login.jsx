59db19f4bead47b411f7b108358bfa6d98fd15b0
unknown <pedrorivdom@gmail.com>
2022-12-19 19:49:33 +0100

    login styles

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='auth'>
+      <form className="auth__form">
+              <div className="auth__form__input">
+                <label>Username</label>
+                <input type="text" />
+              </div>
+              <div className="auth__form__input">
+                <label>Password</label>
+                <input type="text" />
+              </div>
+              <div className="auth__form__btn">
+                <button>Login</button>
+              </div>
+      </form>
+      <div className="auth__image">
+          <div className="auth__image__glass">
+            AQUI VA UN TEXTO
+          </div>
+      </div>
+    </div>
   )
 }
 
