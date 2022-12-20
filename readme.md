54ae1bbb5f02189458d9819e93ab7f6e6d6d5933
unknown <pedrorivdom@gmail.com>
2022-12-20 20:56:27 +0100

    login form

---
+++ readme.md
@@ -0,0 +1,3 @@
+#Mern Project
+
+-React router
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,19 +3,21 @@ import {SiYourtraveldottv } from "react-icons/si";
 const Login = () => {
   return (
     <div className='auth'>
-      <form className="auth__form">
-              <div className="auth__form__input">
-                <label>Username</label>
-                <input type="text" />
-              </div>
-              <div className="auth__form__input">
-                <label>Password</label>
-                <input type="text" />
-              </div>
-              <div className="auth__form__btn">
-                <button>Login</button>
-              </div>
-      </form>
+      <div className="auth__form">
+              <form >
+                  <div className="auth__form__input">
+                    <label>Username</label>
+                    <input type="text" />
+                  </div>
+                  <div className="auth__form__input">
+                    <label>Password</label>
+                    <input type="text" />
+                  </div>
+                  <div className="auth__form__btn">
+                    <button>Login</button>
+                  </div>
+              </form>
+      </div>
       <div className="auth__image">
           <div className="auth__image__glass">
             <p><SiYourtraveldottv/> <br /> Travel Social Nerwork for Lonely <span>People</span></p>
