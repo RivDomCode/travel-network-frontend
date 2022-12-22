f52294f560a19bd4a1af03c992453fec38e3fbb8
unknown <pedrorivdom@gmail.com>
2022-12-22 19:26:47 +0100

    login and register styles

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,30 @@
 import {SiYourtraveldottv } from "react-icons/si";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <div className='auth'>
       <div className="auth__form">
+
               <form >
+              <h3><SiYourtraveldottv/>  Sign In</h3>
                   <div className="auth__form__input">
                     <label>Username</label>
                     <input type="text" />
                   </div>
                   <div className="auth__form__input">
                     <label>Password</label>
-                    <input type="text" />
+                    <input type="password" />
                   </div>
                   <div className="auth__form__btn">
                     <button>Login</button>
                   </div>
+                  <hr />
+                  <Link to="/register">
+                    Not registered yet? Go to register page
+                  </Link>
               </form>
+
       </div>
       <div className="auth__image">
           <div className="auth__image__glass">
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import {SiYourtraveldottv } from "react-icons/si";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='auth'>
+          <div className="auth__image">
+        <div className="auth__image__glass">
+          <p><SiYourtraveldottv/> <br /> Travel Social Nerwork for Lonely <span>People</span></p>
+          <p>The place to connect with others to discover the world</p>
+        </div>
+    </div>
+    <div className="auth__form">
+            <form >
+            <h3><SiYourtraveldottv/> Register</h3>
+                <div className="auth__form__input">
+                  <label>Username</label>
+                  <input type="text" />
+                </div>
+                <div className="auth__form__input">
+                  <label>Password</label>
+                  <input type="password" />
+                </div>
+                <div className="auth__form__input">
+                  <label>Repeat password</label>
+                  <input type="password" />
+                </div>
+                <div className="auth__form__btn">
+                  <button>Login</button>
+                </div>
+                <hr />
+                <Link to="/login">
+                  Already registered? Go to login page
+                </Link>
+            </form>
+
+    </div>
+
+  </div>
   )
 }
 
